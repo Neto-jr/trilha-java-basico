523992720a927a9af80f434d94e867c5e6477cef
Dev Branches <joca_ramosneto@hotmail.com>
2024-03-15 13:08:20 -0300

    atribuindo a classe scanner e locale

---
+++ contaBanco/src/contaTerminal.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class contaTerminal {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        //TODO: Conhecer e importar a classe Scanner
+        Scanner sc = new Scanner(System.in);
+        //Exibri as mensagens para o nosso usuario
+        int numeroConta =1021;
+        String agencia = "067-8";
+        String nomeCliente = "Mario de Andrade";
+        double saldoConta = 237.48;
+        //Obter pelo scanner os valores digitados no terminal
+        System.out.println("Por favor, digite o numero da agência ! ");
+        agencia = sc.next();
+        //Exibir a mensagem conta criada
+
+            System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+
+            agencia+", conta "+numeroConta+" e saldo "+saldoConta+" Já está disponivel para saque");
+        
+        
+    }
+}
---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,27 @@
+
+
+public class Operadores {
+    public static void main(String[] args) {
+        /* String nome = "Jocks";
+        int idade = 33;
+        double peso = 74.4;
+        char sexo = 'M';
+        boolean doadorDeOrgao = true;
+        Date dtNascimento = new Date(); */
+
+        // Operador ternario
+        int a, b;
+        a = 6;
+        b = 6;
+
+        String result = a == b ? "verdadeiro" : "falso";
+
+        // if(a==b){
+        //     result = "verdadeiro";
+        // }else
+        //     result = "falso";
+
+         System.out.println(result);
+        //     System.out.println(dtNascimento);
+    }
+}
